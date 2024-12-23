85873d6b46d60f341bdbde4c1ff943cc9de70344
Maminrina <127506798+henri-aims@users.noreply.github.com>
2024-12-23 11:05:22 -0500

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -10,18 +10,5 @@ document.getElementById('login-form').addEventListener('submit', function(event)
 
     if (username === correctUsername && password === correctPassword) {
         alert("Login successful!");
-        // Redirect to another page, for example:
-        // window.location.href = 'dashboard.html';
     } else {
         document.getElementById('error-message').style.display = 'block';
-    }
-});
-
-// Contact form submission
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-
-    alert("Your message has been sent! We will get back to you shortly.");
-});
-
-
